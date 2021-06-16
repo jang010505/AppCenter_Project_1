5e5cbb52a64c9017a24c13fcb05b877749385e44
hg_yellow <63451752+jang010505@users.noreply.github.com>
2021-06-17 08:23:33 +0900

    Delete Configs.kt

--- app/src/main/java/com/example/appcenter_project_1/model/Configs.kt
+++
@@ -1,6 +0,0 @@
-package com.example.appcenter_project_1.model
-
-object Configs {
-    val clientID = "l2xphJN2SSZ22o1c%2BJOwhWMp3cLDAF6IqHjn28%2BBhfkvYeDVxjC7tOb88JWzSc8ZwijBEpC60AUAENWXbM5o%2BA%3D%3D"
-    val apiKey = "l2xphJN2SSZ22o1c+JOwhWMp3cLDAF6IqHjn28+BhfkvYeDVxjC7tOb88JWzSc8ZwijBEpC60AUAENWXbM5o+A=="
-}
